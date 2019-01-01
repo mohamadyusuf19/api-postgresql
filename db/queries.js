96907487a1bbb0fc5bf05a5f8ee71fb4e7da0ded
ahmad <yusufadinata19@gmail.com>
2019-01-01 22:25:08 +0700

    show one record

--- db/queries.js
+++ db/queries.js
@@ -4,5 +4,8 @@ const knex = require('./knex');
 module.exports = {
     getAll() {
         return knex('sticker');
+    },
+    getOne(id) {
+        return knex('sticker').where('id', id).first();
     }
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,39 @@
+const request = require('supertest');
 const knex = require("../db/knex");
+const expect = require('chai').expect;
+
+const app = require("../app");
 
 describe('CRUD Stickers', () => {
-    before(() => {
+    before((done) => {
         //run migrations
         knex.migrate.latest()
             .then(() => {
                 //run seeds
                 return knex.seed.run()
             }) 
+            .then(() => done());
     })
-}) 
+    it('List all Records', (done) => {
+        request(app)
+            .get('/api/v1/stickers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.a('array');
+                done();
+            });
+    });
+    it('List a record', (done) => {
+        request(app)
+            .get('/api/v1/stickers/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.a('object');
+                done();
+            });
+    });
+});
