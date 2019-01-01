59ac52bb24151c30026a10875d6d560c4d3ea359
ahmad <yusufadinata19@gmail.com>
2019-01-01 19:11:10 +0700

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'cjs-web-store',
+      user:     'postgres',
+      password: 'Etihad@123'
+    }
+  },
+};
